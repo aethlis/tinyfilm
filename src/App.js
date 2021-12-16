9d84ba56bc9e2f6d50671db646d82bed92ba8c56
aethlis <84690253+aethlis@users.noreply.github.com>
2021-12-16 13:35:51 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import Mainpage from "./routes/Mainpage";
 import New from './routes/New';
-import Intropage from './components/Intropage';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Intropage />}>
-        </Route>
-        <Route path="/home" element={<Mainpage />}>
+        <Route path="/" element={<Mainpage />}>
         </Route>
         <Route path="/new" element={<New />}>
          </Route>
--- src/components/Intropage.js
+++ src/components/Intropage.js
@@ -1,13 +1,10 @@
 import styles from "./Intropage.module.css"
-import {Link} from "react-router-dom"
 // import Mainpage from "../routes/Mainpage";
 
 function Intropage() {
     return (
         <div className={styles.fullpage}>
-            <Link to="/home">
-                <div className={styles.introtext}>welcome to tinyfilm</div>
-            </Link>
+            <div className={styles.introtext}>welcome to tinyfilm</div>
         </div>
     )
 }
