418b8543d24981c74f959e9a7fe78a12b6049e0e
aethlis <84690253+aethlis@users.noreply.github.com>
2021-12-17 09:57:53 +0900

    changed some code

---
+++ src/components/MovieGrid.js
@@ -0,0 +1,28 @@
+import styles from "./MovieGrid.module.css"
+
+
+function MovieGrid(id, title, coverImg, summary) {
+    return (
+        // <div className={styles.movie_wrap}>
+        //     <div className={styles.img_wrapper}>
+        //         <img id={styles.coverImg} src={coverImg} alt={id} />
+        //     </div>
+        //     <div className={styles.text_box}>
+        //         <div className={styles.title}>{title}</div>
+        //         <div className={styles.summary}>{summary}</div>
+        //     </div>
+        // </div>
+
+        <div className={styles.movie_wrap}>
+            <div className={styles.img_wrapper}>
+                {/* <img id={styles.coverImg} src={coverImg} alt={id} /> */}
+            </div>
+            <div className={styles.text_box}>
+                <div className={styles.title}> The Dun Dun</div>
+                <div className={styles.summary}>Hi Hello AJDNAJND</div>
+            </div>
+        </div>
+    )
+}
+
+export default MovieGrid;
--- src/routes/Mainpage.js
+++ src/routes/Mainpage.js
@@ -3,6 +3,23 @@ import styles from "./Mainpage.module.css"
 import PlayButton from "../components/PlayButton"
 // import Intropage from "../components/Intropage"
 // import TestImg from "../img/TestImg.png"
+import MovieGrid from "../components/MovieGrid"
+import { useState } from "react"
+
+// function FetchMovie() {
+//     const [movies, setMovies] = useState([])
+//     const [loading, setLoading] = useState("")
+
+//     const [id, setId] = useState("")
+//     const [title, setTitle] = useState("")
+//     const [summary, setSummary] = useState("")
+//     const [coverImg, setImg] = useState("")
+//     const [runtime, setRuntime] = useState("")
+//     const [genres, setGenres] = useState([])
+//     return (
+//         Mainpage(loading, movies)
+//     )
+// }
 
 function ContentAd() {
     return (
@@ -21,13 +38,28 @@ function ContentAd() {
     )
 }
 
-function Mainpage() {
+function Mainpage(loading, movies) {
     return (
         <div>
             <Header />
             <div className={styles.fullpage}>
                 <div className={styles.ContentAd_container}>
                     <ContentAd />
+                </div>
+                {/* <div>
+                    {movies.map((movie) => {
+                        <MovieGrid
+                            // id={}
+                            // coverImg={}
+                            // title={}
+                            // summary={}
+                        />
+                    })}
+                </div> */}
+                <div className={styles.movie_container}>
+                    <div className={styles.movie_wrap}>
+                        <MovieGrid/>
+                    </div>
                 </div>
             </div>
         </div>
