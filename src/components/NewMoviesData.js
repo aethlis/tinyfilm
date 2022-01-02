495bbfa64c8473b1db5f1c1f1baa14b40eb04607
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2022-01-02 16:18:24 +0900

    ㅌㅔ스틋.아무변화없음주의..

--- src/components/NewMoviesData.js
+++ src/components/NewMoviesData.js
@@ -1,12 +1,8 @@
 import { gsap } from "gsap/gsap-core";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect, useRef, useState } from "react";
-import "./NewMoviesData.css"
-// import Swiper from "swiper";
-// import "swiper/css";
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+// import "./NewMoviesData.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 
 function NewMoviesData({ backgroundImg1, backgroundImg2, backgroundImg3, backgroundImg4, backgroundImg5 }) {
@@ -19,38 +15,47 @@ function NewMoviesData({ backgroundImg1, backgroundImg2, backgroundImg3, backgro
     const img4 = useRef();
     const img5 = useRef();
 
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-        gsap.to(img3.current, {
-            scrollTrigger: {
-                trigger: img3,
-                start: "-30px 10%",
-                markers: true,
-                toggleActions: "restart none none none",
-            },
-            rotation: 360,
-            duration: 3,
-        });
-    }, [img3]);
+    // gsap.registerPlugin();
+    // gsap.to(img3.current, {
+    //     scrollTrigger: {
+    //         trigger: img3,
+    //         start: "-30px 10%",
+    //         markers: true,
+    //         toggleActions: "restart none none none",
+    //     },
+    //     rotation: 360,
+    //     duration: 3,
+    // });
 
     return (
-        <div className="imgDiv">
-            <div>
-                <img ref={img1} src={backgroundImg1} className="img1" />
-            </div>
-            <div>
-                <img ref={img2} src={backgroundImg2} className="img2" />
-            </div>
-            <div>
-                <img ref={img3} src={backgroundImg3} className="img3" />
-            </div>
-            <div>
-                <img ref={img4} src={backgroundImg4} className="img4" />
-            </div>
-            <div>
-                <img ref={img5} src={backgroundImg5} className="img5" />
-            </div>
-        </div >
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            <SwiperSlide><img ref={img1} src={backgroundImg1} className="img1" /></SwiperSlide>
+            <SwiperSlide><img ref={img2} src={backgroundImg2} className="img2" /></SwiperSlide>
+            <SwiperSlide><img ref={img3} src={backgroundImg3} className="img3" /></SwiperSlide>
+            <SwiperSlide><img ref={img4} src={backgroundImg4} className="img4" /></SwiperSlide>
+            <SwiperSlide><img ref={img5} src={backgroundImg5} className="img5" /></SwiperSlide>
+        </Swiper>
+
+        /*{ <div>
+            <img ref={img1} src={backgroundImg1} className="img1" />
+        </div>
+        <div>
+            <img ref={img2} src={backgroundImg2} className="img2" />
+        </div>
+        <div>
+            <img ref={img3} src={backgroundImg3} className="img3" />
+        </div>
+        <div>
+            <img ref={img4} src={backgroundImg4} className="img4" />
+        </div>
+        <div>
+            <img ref={img5} src={backgroundImg5} className="img5" />
+        </div> }*/
     );
 }
 
