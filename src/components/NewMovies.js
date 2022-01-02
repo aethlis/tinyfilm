462cf68f59e03b6c9c0a446d7511954902f13dba
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2022-01-03 02:21:37 +0900

    밧줄가져오고..~ 꾸엙ㄹ.

--- src/components/NewMovies.js
+++ src/components/NewMovies.js
@@ -1,25 +1,31 @@
-import gsap from "gsap/gsap-core";
-import propTypes from "prop-types";
-import { useRef } from "react";
+import { useState } from "react";
 import styles from "./NewMovies.module.css"
 
-function NewMovies({ movie1, movie2, movie3, movie4, movie5 }) {
+function NewMovies({ movie1Img, movie2Img, movie3Img, movie4Img, movie5Img, movie3Title, movie3Poster, movie3Rating, movie3RunTime }) {
+    function onMouseOver() {
+        console.log(0);
+        return (
+            < div className={styles.title}>
+                {movie3Title}
+            </div >
+        );
+    }
     return (
         <div>
             <div>
-                <img src={movie1} className={styles.img} />
+                <img src={movie1Img} className={styles.img1} />
             </div>
             <div>
-                <img src={movie2} className={styles.img} />
+                <img src={movie2Img} className={styles.img2} />
             </div>
             <div>
-                <img src={movie3} className={styles.img} />
+                <img src={movie3Img} className={styles.img3} onMouseOver={onMouseOver} />
             </div>
             <div>
-                <img src={movie4} className={styles.img} />
+                <img src={movie4Img} className={styles.img4} />
             </div>
             <div>
-                <img src={movie5} className={styles.img} />
+                <img src={movie5Img} className={styles.img5} />
             </div>
         </div>
     );
--- src/routes/New.js
+++ src/routes/New.js
@@ -5,6 +5,7 @@ import { useMovieAPI } from "../contexts/FetchAPI";
 
 function New() {
     const { movies } = useMovieAPI();
+    console.log(movies);
     if (movies.length < 5) {
         return (<div />)
     }
@@ -12,11 +13,31 @@ function New() {
         <div>
             <Header />
             <NewMovies
-                movie1={movies[0].background_image}
-                movie2={movies[1].background_image}
-                movie3={movies[2].background_image}
-                movie4={movies[3].background_image}
-                movie5={movies[4].background_image}
+                movie1Img={movies[0].background_image}
+                movie2Img={movies[1].background_image}
+                movie3Img={movies[2].background_image}
+                movie4Img={movies[3].background_image}
+                movie5Img={movies[4].background_image}
+                movie1Title={movies[0].title}
+                movie2Title={movies[1].title}
+                movie3Title={movies[2].title}
+                movie4Title={movies[3].title}
+                movie5Title={movies[4].title}
+                movie1Poster={movies[0].medium_cover_image}
+                movie2Poster={movies[1].medium_cover_image}
+                movie3Poster={movies[2].medium_cover_image}
+                movie4Poster={movies[3].medium_cover_image}
+                movie5Poster={movies[4].medium_cover_image}
+                movie1Rating={movies[0].rating}
+                movie2Rating={movies[1].rating}
+                movie3Rating={movies[2].rating}
+                movie4Rating={movies[3].rating}
+                movie5Rating={movies[4].rating}
+                movie1RunTime={movies[0].runtime}
+                movie2RunTime={movies[1].runtime}
+                movie3RunTime={movies[2].runtime}
+                movie4RunTime={movies[3].runtime}
+                movie5RunTime={movies[4].runtime}
             />
         </div >
     );
