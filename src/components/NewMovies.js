31c013ab2c3eb427cbdd365a3017b1c9d0efc054
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2022-01-04 07:34:42 +0900

    어머니..제가 희망이란것을 좀가져도괜찮겟습니까.?

--- src/components/NewMovies.js
+++ src/components/NewMovies.js
@@ -3,39 +3,34 @@ import { useRef, useEffect } from "react";
 import styles from "./NewMovies.module.css"
 
 function NewMovies({ movie1Img, movie2Img, movie3Img, movie4Img, movie5Img, movie3Title, movie3Poster, movie3Rating, movie3RunTime }) {
-    const img2 = useRef(null);
-    const img5 = useRef(null);
-    useEffect(() => {
-        gsap.to(img5.current, {
-            x: 100,
-            duration: 2,
-            opacity: 1,
-            scrollTrigger: {
-                trigger: img5,
-                markers: true,
-                start: "top",
-                end: "bottom",
-                toggleActions: "restart none none none",
-            },
-        })
-    }, []);
+    // useEffect(() => {
+    //     gsap.from(".movie5", {
+    //         y: 100,
+    //         opacity: 0,
+    //         delay: 1,
+    //         duration: 1,
+    //         scrollTrigger: {
+    //             trigger: ".movie5"
+    //         }
+    //     })
+    // }, []);
     return (
         <div className={styles.fullPage}>
             <div className={styles.imgDiv}>
                 <div>
-                    <img src={movie1Img} />
+                    <img src={movie1Img} className="movie1" />
                 </div>
                 <div>
-                    <img src={movie2Img} ref={img2} />
+                    <img src={movie2Img} className="movie2" />
                 </div>
                 <div>
-                    <img src={movie3Img} />
+                    <img src={movie3Img} className="movie3" />
                 </div>
                 <div>
-                    <img src={movie4Img} />
+                    <img src={movie4Img} className="movie4" />
                 </div>
                 <div>
-                    <img src={movie5Img} ref={img5} />
+                    <img src={movie5Img} className="movie5" className={styles.movie5} />
                 </div>
             </div>
         </div>
