7e83593963563608b2fe34bff15d189de93b4564
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2022-01-03 12:21:55 +0900

    아랏다.

--- src/components/NewMovies.js
+++ src/components/NewMovies.js
@@ -1,52 +1,41 @@
+import gsap from "gsap";
 import { useRef, useEffect } from "react";
-import { gsap } from "gsap";
-import { Draggable } from "gsap/Draggable";
 import styles from "./NewMovies.module.css"
 
-gsap.registerPlugin(Draggable);
-
 function NewMovies({ movie1Img, movie2Img, movie3Img, movie4Img, movie5Img, movie3Title, movie3Poster, movie3Rating, movie3RunTime }) {
-    const dragInstance = useRef(null);
-    const dragTarget = useRef(null);
-    const dragTarget1 = useRef(null);
-    const dragTarget2 = useRef(null);
-    const dragTarget3 = useRef(null);
-    const dragTarget4 = useRef(null);
-
+    const img2 = useRef(null);
+    const img5 = useRef(null);
     useEffect(() => {
-        dragInstance.current = Draggable.create(dragTarget.current, {
-            type: "y",
-        });
-        dragInstance.current = Draggable.create(dragTarget1.current, {
-            type: "y",
-        });
-        dragInstance.current = Draggable.create(dragTarget2.current, {
-            type: "y",
-        });
-        dragInstance.current = Draggable.create(dragTarget3.current, {
-            type: "y",
-        });
-        dragInstance.current = Draggable.create(dragTarget4.current, {
-            type: "y",
-        });
+        gsap.to("#img5", {
+            x: 100,
+            duration: 2,
+            opacity: 1,
+            scrollTrigger: {
+                trigger: "#img5",
+                markers: true,
+                start: "top",
+                end: "bottom",
+            },
+        })
     }, []);
-
     return (
-        <div>
-            <div>
-                <img src={movie1Img} className={styles.img1} ref={dragTarget} />
-            </div>
-            <div>
-                <img src={movie2Img} className={styles.img2} ref={dragTarget1} />
-            </div>
-            <div>
-                <img src={movie3Img} className={styles.img3} ref={dragTarget2} />
-            </div>
-            <div>
-                <img src={movie4Img} className={styles.img4} ref={dragTarget3} />
-            </div>
-            <div>
-                <img src={movie5Img} className={styles.img5} ref={dragTarget4} />
+        <div className={styles.fullPage}>
+            <div className={styles.imgDiv}>
+                <div>
+                    <img src={movie1Img} />
+                </div>
+                <div>
+                    <img src={movie2Img} ref={img2} />
+                </div>
+                <div>
+                    <img src={movie3Img} />
+                </div>
+                <div>
+                    <img src={movie4Img} />
+                </div>
+                <div>
+                    <img src={movie5Img} id={img5} />
+                </div>
             </div>
         </div>
     );
