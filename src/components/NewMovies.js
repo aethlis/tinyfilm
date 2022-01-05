ce2bc484bfc95ed4c9232668fcdae97e46a8f6fd
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2022-01-06 04:36:56 +0900

    허겅...

--- src/components/NewMovies.js
+++ src/components/NewMovies.js
@@ -7,58 +7,53 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function NewMovies({ movie1BgImg, movie1Poster, movie1Title, movie1Rating, movie1RunTime, movie2BgImg, movie2Title, movie2Poster, movie2Rating, movie2RunTime, movie3BgImg, movie3Title, movie3Poster, movie3Rating, movie3RunTime, movie4BgImg, movie4Title, movie4Poster, movie4Rating, movie4RunTime, movie5BgImg, movie5Title, movie5Poster, movie5Rating, movie5RunTime }) {
-    // const imgDiv = useRef();
-    // const movie1 = useRef();
-    // const movie2 = useRef();
-    // const movie3 = useRef();
-    // const movie4 = useRef();
-    // const movie5 = useRef();
+    const bgImg1 = useRef();
+    const bgImg2 = useRef();
+    const bgImg3 = useRef();
+    const bgImg4 = useRef();
+    const bgImg5 = useRef();
 
-    // const bGMovie1 = useRef();
-    // const bGMovie2 = useRef();
-    // const bGMovie3 = useRef();
-    // const bGMovie4 = useRef();
-    // const bGMovie5 = useRef();
+    const movie1Data = useRef();
+    const movie2Data = useRef();
+    const movie3Data = useRef();
+    const movie4Data = useRef();
+    const movie5Data = useRef();
 
-    // useEffect(() => {
-    //     gsap.to(movie2.current, {
-    //         y: -255,
-    //         scrollTrigger: {
-    //             markers: true,
-    //             trigger: bGMovie2.current,
-    //             toggleActions: "restart none reverse none"
-    //         },
-    //     })
-    //     gsap.to(movie3.current, {
-    //         y: -510,
-    //         scrollTrigger: {
-    //             markers: true,
-    //             trigger: bGMovie3.current,
-    //             toggleActions: "restart none reverse none"
-    //         },
-    //     })
-    // }, [])
+    useEffect(() => {
+        gsap.to(bgImg2.current, {
+            zIndex: 99,
+            delay: 0.3,
+            scrollTrigger: {
+                trigger: movie2Data.current,
+                start: "30% center",
+                toggleActions: "restart none resume none",
+                markers: true,
+            }
+        })
+    }, [])
 
     return (
         <div>
-            <section>
-                <img src={movie1BgImg} className={styles.bgImg} />
-            </section>
-            <section>
-                <img src={movie2BgImg} className={styles.bgImg} />
-            </section>
-            <section>
-                <img src={movie3BgImg} className={styles.bgImg} />
-            </section>
-            <section>
-                <img src={movie4BgImg} className={styles.bgImg} />
-            </section>
-            <section>
-                <img src={movie5BgImg} className={styles.bgImg} />
-            </section>
-            <section className={styles.bgB}>
-            </section>
-            <div className={styles.movie1Data}>
+            <div className={styles.bgImg}>
+                <section>
+                    <img src={movie1BgImg} className={styles.bgImg1} ref={bgImg1} />
+                </section>
+                <section>
+                    <img src={movie2BgImg} className={styles.bgImg2} ref={bgImg2} />
+                </section>
+                <section>
+                    <img src={movie3BgImg} className={styles.bgImg3} ref={bgImg3} />
+                </section>
+                <section>
+                    <img src={movie4BgImg} className={styles.bgImg4} ref={bgImg4} />
+                </section>
+                <section>
+                    <img src={movie5BgImg} className={styles.bgImg5} ref={bgImg5} />
+                </section>
+                <section className={styles.bgB}>
+                </section>
+            </div>
+            <div className={styles.movie1Data} ref={movie1Data}>
                 <div className={styles.movieContent}>
                     <div className={styles.moviePoster}>
                         <img src={movie1Poster} />
@@ -76,7 +71,7 @@ function NewMovies({ movie1BgImg, movie1Poster, movie1Title, movie1Rating, movie
                     </div>
                 </div>
             </div>
-            <div className={styles.movie2Data}>
+            <div className={styles.movie2Data} ref={movie2Data}>
                 <div className={styles.movieContent}>
                     <div className={styles.moviePoster}>
                         <img src={movie2Poster} />
@@ -94,7 +89,7 @@ function NewMovies({ movie1BgImg, movie1Poster, movie1Title, movie1Rating, movie
                     </div>
                 </div>
             </div>
-            <div className={styles.movie3Data}>
+            <div className={styles.movie3Data} ref={movie3Data}>
                 <div className={styles.movieContent}>
                     <div className={styles.moviePoster}>
                         <img src={movie3Poster} />
@@ -111,7 +106,8 @@ function NewMovies({ movie1BgImg, movie1Poster, movie1Title, movie1Rating, movie
                         </div>
                     </div>
                 </div>
-            </div><div className={styles.movie4Data}>
+            </div>
+            <div className={styles.movie4Data} ref={movie4Data}>
                 <div className={styles.movieContent}>
                     <div className={styles.moviePoster}>
                         <img src={movie4Poster} />
@@ -128,7 +124,7 @@ function NewMovies({ movie1BgImg, movie1Poster, movie1Title, movie1Rating, movie
                         </div>
                     </div>
                 </div>
-            </div><div className={styles.movie5Data}>
+            </div><div className={styles.movie5Data} ref={movie5Data}>
                 <div className={styles.movieContent}>
                     <div className={styles.moviePoster}>
                         <img src={movie5Poster} />
@@ -147,45 +143,6 @@ function NewMovies({ movie1BgImg, movie1Poster, movie1Title, movie1Rating, movie
                 </div>
             </div>
         </div>
-        // <div >
-        //     <div className={styles.fullPage}>
-        //         <div className={styles.BGImg}>
-        //             <section>
-        //                 <img src={movie1Img} ref={bGMovie1} className={styles.bGMovie1} />
-        //             </section>
-        //             <section>
-        //                 <img src={movie2Img} ref={bGMovie2} />
-        //             </section>
-        //             <section>
-        //                 <img src={movie3Img} ref={bGMovie3} />
-        //             </section>
-        //             <section>
-        //                 <img src={movie4Img} ref={bGMovie4} />
-        //             </section>
-        //             <section>
-        //                 <img src={movie5Img} ref={bGMovie5} />
-        //             </section>
-        //         </div>
-        //         <div className={styles.bGB}></div>
-        //         <div className={styles.imgDiv} ref={imgDiv} >
-        //             <section>
-        //                 <img src={movie1Img} className={styles.movie1} ref={movie1} />
-        //             </section>
-        //             <section>
-        //                 <img src={movie2Img} className={styles.movie2} ref={movie2} />
-        //             </section>
-        //             <section>
-        //                 <img src={movie3Img} className={styles.movie3} ref={movie3} />
-        //             </section>
-        //             <section>
-        //                 <img src={movie4Img} className={styles.movie4} ref={movie4} />
-        //             </section>
-        //             <section>
-        //                 <img src={movie5Img} className={styles.movie5} ref={movie5} />
-        //             </section>
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
 
